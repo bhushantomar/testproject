13c15db8077772ce0c5492b0a0d56235b58945af
Bhushan Tomar <bhushantomar@Bhushans-MBP.attlocal.net>
2024-09-21 17:54:35 -0500

    Updated greeting function

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("this is test project")
+# main.py
+def greet(name):
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    print(greet(user_name))
